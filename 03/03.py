06e9a89f81027ad66352a8010353dcbfdb5e5201
Steven Clark <sclark@exptechinc.com>
2021-12-03 13:56:28 -0500

    day 3 part 2

--- 03/03.py
+++ 03/03.py
@@ -1,21 +1,51 @@
 import numpy as np
 
-fn = 'input.txt'
-#fn = 'test.txt'
+def filter_data(data, use_most_common):
+    _, nc = data.shape
+    for c in range(nc):
+        nr, _ = data.shape
+        if nr <= 1:
+            break
+        
+        col_score = sum(data[:,c])/nr
+        if use_most_common:
+            keep_val = col_score >= 0.5
+        else:
+            keep_val = col_score < 0.5
 
-lines = np.loadtxt(fn, dtype=str)
-num_lines = len(lines)
+        mask = data[:,c] == keep_val
+        data = data[mask, :]
 
-data = np.array([[int(c) for c in s] for s in lines])
+    x = 0
+    for n in data[0,:]:
+        x = (x << 1) + n
 
-gamma_list = (np.sum(data, axis=0)/num_lines > 0.5).astype(int)
-gamma = 0
-epsilon = 0
-for n in gamma_list:
-    gamma = (gamma << 1) + n
-    epsilon = (epsilon << 1) + (1-n)
-
-print(gamma*epsilon)
+    return x
 
 
-j
+def main():
+    fn = 'input.txt'
+    #fn = 'test.txt'
+
+    lines = np.loadtxt(fn, dtype=str)
+    num_lines = len(lines)
+
+    data = np.array([[int(c) for c in s] for s in lines])
+
+    gamma_list = (np.sum(data, axis=0)/num_lines > 0.5).astype(int)
+    gamma = 0
+    epsilon = 0
+    for n in gamma_list:
+        gamma = (gamma << 1) + n
+        epsilon = (epsilon << 1) + (1-n)
+
+    print(gamma*epsilon)
+
+    rating1 = filter_data(data, use_most_common=True)
+    rating2 = filter_data(data, use_most_common=False)
+
+    print(rating1*rating2)
+
+
+if __name__ == '__main__':
+    main()
