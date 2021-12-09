d02a82a7e0c6676fabeafb5d321c60f719a128be
Steven Clark <sclark@exptechinc.com>
2021-12-09 10:22:42 -0500

    sean plott

--- 09/09.py
+++ 09/09.py
@@ -1,12 +1,42 @@
 import numpy as np
 from scipy.signal import argrelextrema, argrelmin #thanks stackoverflow!
 
-fn = 'test.txt'
+#fn = 'test.txt'
+fn = 'input.txt'
 
 with open(fn, 'r') as f:
     lines = [list(map(int, list(li.strip()))) for li in f.readlines()]
 
 g = np.array(lines)
-print(g)
+nr,nc = g.shape
+dtype = g.dtype
 
-print(argrelmin(g))
+#frame with nines
+g2 = np.ones((nr+2,nc+2), dtype=dtype)*9
+g2[1:nr+1,1:nc+1] = g
+g = g2
+
+vs = set(zip(*argrelmin(g, mode='clip', axis=0)))
+hs = set(zip(*argrelmin(g, mode='clip', axis=1)))
+lows = set.intersection(vs, hs)
+
+risk = 0
+for r,c in lows:
+    risk += (1+g[r,c])
+
+print(risk)
+
+def flood(g, r, c, visited):
+    if g[r,c] == 9 or (r,c) in visited:
+        return 0
+    visited.add((r,c))
+    return 1 + (flood(g,r+1,c,visited) + flood(g,r-1,c,visited) +
+                flood(g,r,c+1,visited) + flood(g,r,c-1,visited))
+
+basin_sizes = list()
+for r,c in lows:
+    visited = set()
+    basin_sizes.append(flood(g,r,c,visited))
+
+basin_sizes.sort()
+print(np.prod(basin_sizes[-3:]))
